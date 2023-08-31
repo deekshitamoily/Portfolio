4a8442493672b31dbe0cd842d9eb3cb5d0ffcabf
deekshita <deekshitamoily@gmail.com>
2023-08-31 11:05:05 +0530

    Add description on the new columns.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("Images/photo.png")
+    st.image("Images/photo.png",width=200)
 
 with col2:
     st.title("Deekshita Moily")
@@ -11,5 +11,8 @@ with col2:
               Hi All, I'm a recent  BCA graduate who is really exited about creating apps. 
               With a strong foundation in Python programming, I'm on my way to becoming a proficient Python programmer.
               My goal is to keep growing in theworld of app development.
-                """
+              """
     st.info(content)
+content1 = """
+Below you can find some of the apps that I have built in Python.Feel free to contact me!"""
+st.info(content1)
