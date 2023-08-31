87209a95b14cfc5ea75d89b0d96b7cf85475b245
deekshita <deekshitamoily@gmail.com>
2023-08-31 13:08:30 +0530

    Add description on the new columns,and links.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image("Images/photo.png",width=200)
+    st.image("Images/photo.png",width=250)
 
 with col2:
     st.title("Deekshita Moily")
@@ -16,3 +17,24 @@ with col2:
 content1 = """
 Below you can find some of the apps that I have built in Python.Feel free to contact me!"""
 st.info(content1)
+
+#responsive or provide space between the rows!
+col3, empty_col,col4 = st.columns([1.5,0.5,1.5])
+col3,col4 =st.columns(2)
+#resposive or provide space between the rows!
+
+df = pandas.read_csv("data (1).csv",sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("Images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("Images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
